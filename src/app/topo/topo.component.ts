f53a82696001c4144c226976242f43b5e05f523d
Arthur Barbosa <arthurbruno03@gmail.com>
2020-08-22 21:31:16 -0400

    [added]  component tentativas

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 
+  public title: string = "Aprendendo english"
   constructor() { }
 
   ngOnInit(): void {
