39e3dd5184092e0dc1bdd3d8f57584836de7c029
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-09 10:55:03 -0400

    [added] mock frases

--- src/app/painel/frase-mock.ts
+++ src/app/painel/frase-mock.ts
@@ -1,9 +1,9 @@
 import {Frase} from '../shared/frase.model'
 
-const FRASES: Frase[] = [
+export const FRASES: Frase[] = [
     {fraseEng: 'I like to learn', frasePort: 'Eu gosto  de aprender'},
     {fraseEng: 'I watch tv', frasePort: 'Eu assisto tv'},
-    {fraseEng: 'How are you', frasePort: 'Como vai você'},
+    {fraseEng: 'How are you?', frasePort: 'Como vai você?'},
     {fraseEng: 'I eat bread', frasePort: 'Eu como pão'},
     
 ]
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 
-  public title: string = "Aprendendo english"
+  public title: string = "Learning english"
   constructor() { }
 
   ngOnInit(): void {
