334f9266f62197820ba01e9965638e8d34ea403e
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-09 11:58:41 -0400

    [added] comparando as frases

--- src/app/painel/frase-mock.ts
+++ src/app/painel/frase-mock.ts
@@ -1,7 +1,7 @@
 import {Frase} from '../shared/frase.model'
 
 export const FRASES: Frase[] = [
-    {fraseEng: 'I like to learn', frasePort: 'Eu gosto  de aprender'},
+    {fraseEng: 'I like to learn', frasePort: 'Eu gosto de aprender'},
     {fraseEng: 'I watch tv', frasePort: 'Eu assisto tv'},
     {fraseEng: 'How are you?', frasePort: 'Como vai você?'},
     {fraseEng: 'I eat bread', frasePort: 'Eu como pão'},
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,11 +10,41 @@ import { FRASES } from './frase-mock';
 export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
-  public instrucao: string = 'Traduza a frase.'
+  public instrucao: string = 'Traduza a frase.';
+  public resposta: string;
 
-  constructor() { console.log(this.frases)}
+  public rodada: number = 0;
+  public rodadeFrase: Frase;
+
+  constructor() {
+    this.rodadeFrase = this.frases[this.rodada];
+    console.log(this.rodadeFrase)
+  }
 
   ngOnInit(): void {
   }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    //console.log(this.resposta);
+  }
+
+  public verificaResposta(): void {
+
+    if (this.rodadeFrase.frasePort == this.resposta) {
+      // trocar a pergunta da rodada
+      this.rodada++;
+
+      // atualiza o objeto da rodade
+      this.rodadeFrase = this.frases[this.rodada]
+      alert('Resposta correta')
+
+    } else {
+      alert('Resposta incorreta.')
+    }
+
+  }
+
+
 
 }
