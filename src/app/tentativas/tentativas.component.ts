8267a1430c847126c1b41daf31437162620641c7
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-09 16:53:16 -0400

    capturando as alteracoes do componente pai para o filho

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Coracao } from '../shared/coracao.model';
 
 @Component({
@@ -6,20 +6,24 @@ import { Coracao } from '../shared/coracao.model';
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
-  public coracaoVazio: string = '/assets/coracao_vazio.png'
-  public coracaoCheio: string = '/assets/coracao_cheio.png'
+  @Input() public tentativas: number;
 
   public coracoes: Coracao[] = [
     new Coracao(true), new Coracao(true), new Coracao(true)
   ]
 
-  constructor() { 
+  constructor() {
     console.log(this.coracoes)
   }
 
+  ngOnChanges(){
+    console.log(this.tentativas)
+  }
+
   ngOnInit(): void {
+   
   }
 
 }
