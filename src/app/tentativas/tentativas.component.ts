a5971f8d5bf3188745c2b1018b9636747eedc919
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-13 07:53:20 -0400

    controlando as tentativas

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -19,6 +19,11 @@ export class TentativasComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(){
+    if(this.tentativas !== this.coracoes.length){
+      let indice = this.coracoes.length - this.tentativas;
+
+      this.coracoes[indice - 1].cheio = false;
+    }
     console.log(this.tentativas)
   }
 
